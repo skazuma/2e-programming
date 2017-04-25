5983f886f4b52d00b4c194aca72b4f1aaa778160
skazuma <e1618@s.akashi.ac.jp>
2017-04-25 19:24:19 +0900

    <new files>

---
+++ aaa.c
@@ -0,0 +1,8 @@
+#include <stdi.h>
+
+int main(int argc, char **argv)
+{
+  printf("aaaaaaaaaaaa\n")  
+
+  return 0;
+}
---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+  // この部分に命令を述べる（他は毎回同じ）
+  printf("Hello,World!.\n");
+
+  return(0);
+}
