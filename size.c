1919851c05e588be553874ffd4cd19840e619f67
skazuma <e1618@s.akashi.ac.jp>
2017-04-27 17:06:50 +0900

    <koredesaigo>

--- size.c
+++ size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 
 int main(int argc, char **argv)
@@ -15,20 +16,22 @@ int main(int argc, char **argv)
   printf("unsigned long型:%zuバイト\n",sizeof(unsigned long));
   printf("unsigned long long型:%zuバイト\n",sizeof(unsigned long long));
   printf("char型:%zuバイト\n",sizeof(char));
+  printf("intmax_t型:%zuバイト\n",sizeof(intmax_t));
+  printf("uintmax_t型:%zuバイト\n",sizeof(uintmax_t));
+  printf("intptr_t型:%zuバイト\n",sizeof(intptr_t));
+  printf("uintptr_t型:%zuバイト\n",sizeof(uintptr_t));
+  printf("float型:%zuバイト\n",sizeof(float));
+  printf("double型:%zuバイト\n",sizeof(double));
+  printf("long double型:%zuバイト\n",sizeof(long double));
   /*
+  下記の型の場合は、自分の環境ではコンパイルできないようです。
   printf("intN_t型:%zuバイト\n",sizeof(intN_t));
   printf("uintN_t型:%zuバイト\n",sizeof(uintN_t));
   printf("int_leastN_t型:%zuバイト\n",sizeof(int_leastN_t));
   printf("uint_leastN_t型:%zuバイト\n",sizeof(uint_leastN_t));
   printf("int_fastN_t型:%zuバイト\n",sizeof(int_fastN_t));
   printf("uint_fastN_t型:%zuバイト\n",sizeof(uint_fastN_t));
-  printf("intmax_t型:%zuバイト\n",sizeof(intmax_t));
-  printf("uintmax_t型:%zuバイト\n",sizeof(uintmax_t));
-  printf("intptr_t型:%zuバイト\n",sizeof(intptr_t));
-  printf("uintptr_t型:%zuバイト\n",sizeof(uintptr_t));
   */
-  printf("float型:%zuバイト\n",sizeof(float));
-  printf("double型:%zuバイト\n",sizeof(double));
-  printf("long double型:%zuバイト\n",sizeof(long double));
+
   return 0;
 }
