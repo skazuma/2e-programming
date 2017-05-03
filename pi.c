4dfdf790bfbfbb71a3edf519a9616467462dc419
skazuma <e1618@s.akashi.ac.jp>
2017-05-03 19:55:53 +0900

    訂正済み

--- pi.c
+++ pi.c
@@ -3,17 +3,25 @@
 
 int main(int argc, char **argv)
 {
-  double pi=3.1415;
-  double a=pi*2.0;
-  printf("小数点以下1桁まで表示\n");
-  printf("円周:%0.1fr\n",a);
-  printf("円の面積:%0.1fr^2\n",pi);
-  printf("小数点以下2桁まで表示\n");
-  printf("円周:%0.2fr\n",a);
-  printf("円の面積:%0.2fr^2\n",pi);
-  printf("小数点以下3桁まで表示\n");
-  printf("円周:%0.3fr\n",a);
-  printf("円の面積:%0.3fr^2\n",pi);
+	float pi = 3.1415;
+	float r,c,s;
 
-  return 0;
+	printf("円の半径を入力してください（小数点以下を含めて入力）:");
+	scanf("%f",&r);
+	c=2.0*pi*r;
+	s=pi*r*r;
+
+
+	printf("小数点以下１桁を出力\n");
+	printf("円周:%0.1f\n",c);
+	printf("面積:%0.1f\n\n",s);
+	printf("小数点以下2桁を出力\n");
+	printf("円周:%0.2f\n",c);
+	printf("面積:%0.2f\n\n",s);
+	printf("小数点以下3桁を出力\n");
+	printf("円周:%0.3f\n",c);
+	printf("面積:%0.3f\n",s);
+
+
+	return 0;
 }
